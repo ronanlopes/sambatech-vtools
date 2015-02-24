e8827742dff23cb2497d85132e9ccdaea9c41148
ronanlopes <lopesronanufsj@gmail.com>
2015-02-23 21:27:56 -0300

    Ajuste de urls

--- sambatechvtools/urls.py
+++ sambatechvtools/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'sambatechvtools.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^s3direct/', include('s3direct.urls')),
-    url(r'^$', 'sambatechvtools.views.index'),
+    url(r'^$', include('converter.urls')),
     url(r'^converter/', include('converter.urls')),
     #url(r'^admin/', include(admin.site.urls)),
 )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
